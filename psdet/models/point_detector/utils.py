f9ee4fae8009ad56406efde97228636306e67465
chaytonmin <969617694@qq.com>
2020-11-25 12:39:37 +0800

    update backbone

--- psdet/models/point_detector/utils.py
+++ psdet/models/point_detector/utils.py
@@ -99,15 +99,7 @@ class VGG(nn.Module):
     def __init__(self, features, num_classes=1000, init_weights=True):
         super(VGG, self).__init__()
         self.features = features
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
+
         if init_weights:
             self._initialize_weights()
 
